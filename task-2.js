a2584ea67ef7c48659823c318b437b6a8a317b4b
arishelka <arishelka181@gmail.com>
2025-05-03 16:36:31 +0200

    add changes

---
+++ task-2.js
@@ -0,0 +1,19 @@
+class Storage {
+    #items;
+
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(item => item !== itemToRemove);
+    }
+}
